df7ff6ac042498d3447e124c56a12a1a5b9b26b1
Alex Matos <99854820+AlexMatosWeb@users.noreply.github.com>
2023-05-29 16:27:24 -0300

    teste

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -13,22 +13,22 @@ export default function Skills() {
   const skills = [
     {
       name: "HTML5/CSS3",
-      icon: "src/images/html.svg",
+      icon: "/src/images/html.svg",
       level: 95,
     },
     {
       name: "JavaScript",
-      icon: "src/images/javascript.svg",
+      icon: "/src/images/javascript.svg",
       level: 90,
     },
     {
       name: "React.js",
-      icon: "src/images/react.svg",
+      icon: "/src/images/react.svg",
       level: 85,
     },
     {
       name: "Node.js",
-      icon: "src/images/nodejs.svg",
+      icon: "/src/images/nodejs.svg",
       level: 65,
     },
   ]
